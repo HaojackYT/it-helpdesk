c10671cb5dfde55e718fe9d14422221752a194ab
HaojackYT <tangchihao4954@gmail.com>
2026-01-01 12:41:03 +0700

    feat(controller): add page controller

---
+++ src/main/java/com/ithd/it_helpdesk/controller/PageController.java
@@ -0,0 +1,33 @@
+package com.ithd.it_helpdesk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping({"/", "/login"})
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+    
+    @GetMapping("/admin/dashboard")
+    public String adminDashboard() {
+        return "admin/admin-dashboard";
+    }
+    
+    @GetMapping("/support/dashboard")
+    public String supportDashboard() {
+        return "support/support-dashboard";
+    }
+    
+    @GetMapping("/employee/dashboard")
+    public String employeeDashboard() {
+        return "employee/employee-dashboard";
+    }
+}
